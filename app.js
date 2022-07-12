ba8b7ef284dcb6fbabbf07b342c470a03d979733
Aaditya Khantal <khantalak2609@gmail.com>
2022-07-12 11:38:23 +0530

    Styling The Page

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var buttonTranslate=document.querySelector("#translate");
 var textInput=document.querySelector("#textArea");
 var output=document.querySelector("#output")
+var url="https://api.funtranslations.com/translate/minion.json";
+
+function getUrl(translationtext)
+{
+   return url+"?"+"text="+translationtext
+}
+
+function errorHandler(error)
+{
+   console.log("Some Error has occured "+ error);
+   alert("Something Went Wrong!! Please Try After Sometime");
+}
 function clickEventHandler()
 {
-   output.innerText="Translated Text :"+textInput.value;
+   var input=textInput.value;
+   fetch(getUrl(input))
+      .then(response=>response.json())
+      .then(json=>{
+         var translatedText=json.contents.translated
+            output.innerText=translatedText;
+      })
+      .catch(errorHandler)
 }
 buttonTranslate.addEventListener("click",clickEventHandler);
